05b8df76dda308ab008d027adc0f679bd54efe91
Mel Michael <mel.michael007@gmail.com>
2021-08-15 11:11:27 -0700

    Add prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="container pt-3">
-
       <header>
         <h3>The Breaking Bad Movie</h3>
       </header>
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a
-        className="btn btn-primary"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a className="btn btn-primary" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
       <form className="d-flex mt-3">
         <input className="form-control me-2" type="search" placeholder="Search Characters" aria-label="Search" />
-        <button className="btn btn-outline-info" type="submit">Search</button>
+        <button className="btn btn-outline-info" type="submit">
+          Search
+        </button>
       </form>
 
-      <div>
-
-      </div>
+      <div></div>
 
       <div className="row py-4">
-    <div className="col border m-3">col</div>
-    <div className="col border m-3">col</div>
-    <div className="col border m-3">col</div>
-    <div className="col border m-3">col</div>
-  </div>
+        <div className="col border rounded shadow-sm m-3 p-2">col</div>
+        <div className="col border rounded shadow-sm m-3 p-2">col</div>
+        <div className="col border rounded shadow-sm m-3 p-2">col</div>
+        <div className="col border rounded shadow-sm m-3 p-2">col</div>
+      </div>
     </div>
   );
 }
