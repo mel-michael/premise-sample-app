918e32aeb9532f875e47ce4d5d8dbb18eb4c49ff
Mel Michael <mel.michael007@gmail.com>
2021-08-15 14:16:23 -0700

    Implement search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 
 // styles
 import './App.css';
@@ -27,17 +27,42 @@ type Character = {
   better_call_saul_appearance: number[];
 };
 
-const API = 'https://www.breakingbadapi.com/api';
+const API_URL = 'https://www.breakingbadapi.com/api';
+
 function App() {
   const [list, setList] = useState<Character[]>([]);
+  const [filteredData, setFiltered] = useState<Character[]>([]);
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchInput(event.target.value);
+
+    if (value.length === 0) {
+      setFiltered(list);
+    }
+    if (value.length >= 3) {
+      handleNameSearch(value);
+    }
+  };
+
+  const handleNameSearch = (data: string) => {
+    const searchRegex = new RegExp(data, 'i');
+    const result = list.filter(({ name }) => {
+      if (name.match(searchRegex)) return true;
+      return false;
+    });
+    setFiltered(result);
+  };
 
   useEffect(() => {
     const getAllChars = async () => {
-      fetch(`${API}/characters`)
+      fetch(`${API_URL}/characters`)
         .then((response) => response.json())
         .then((data) => {
           console.log('DATA::', data);
           setList(data);
+          setFiltered(data);
         });
     };
 
@@ -57,43 +82,52 @@ function App() {
   return (
     <div className="container pt-3">
       <header>
-        <h3>The Breaking Bad Movie</h3>
+        <h3>The Breaking Bad TV Series</h3>
       </header>
-      <form className="d-flex my-3">
-        <input className="form-control me-2" type="search" placeholder="Search Characters" aria-label="Search" />
-        <button className="btn btn-outline-info" type="submit">
-          Search
-        </button>
-      </form>
+      <div className="row">
+        <form className="d-flex my-3 col pl-0 ml-3">
+          <input
+            value={searchInput}
+            onChange={handleChange}
+            className="form-control me-2"
+            type="search"
+            placeholder="Search Characters by name"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-info" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="col" />
+      </div>
 
-      <div className="row py-4">
-        {list.map((option) => {
+      <div className="row pb-6">
+        <h5 className="m-3 p-0">All Characters</h5>
+        {filteredData.map((option) => {
           const { char_id, name, img, occupation, status, nickname } = option;
           return (
-            <div
-              style={{ height: 420 }}
-              key={char_id}
-              className="position-relative col-2 border rounded shadow-sm m-3 mb-4 p-0"
-            >
-              <img src={img} alt="Char" className="img-fluid rounded-top w-100 h-100" style={{ maxHeight: 300 }} />
-              <h6 className="character-name">{name}</h6>
-              <div className="d-flex justify-content-between align-items-center p-2">
-                <p className="mb-0 font-weight-bold" style={{ fontSize: 12 }}>
-                  Nickname: {nickname}
-                </p>
-                <p className="mb-0">
-                  {status === Status[0] ? (
-                    <AliveIcon width={18} fill="#20c997" />
-                  ) : (
-                    <DeadIcon width={18} fill="#dc3545" />
-                  )}
-                </p>
-              </div>
-              <div className="p-2" style={{ fontSize: 14 }}>
-                <p className="m-0" style={{ fontSize: 12 }}>
-                  Occupation:
-                </p>
-                <Occupation jobs={occupation} />
+            <div key={char_id} className="col-3 mb-4">
+              <div style={{ height: 540 }} className="position-relative border rounded shadow-sm p-0">
+                <img src={img} alt="Char" className="img-fluid rounded-top w-100 h-100" style={{ maxHeight: 420 }} />
+                <h6 className="character-name">{name}</h6>
+                <div className="d-flex justify-content-between align-items-center px-3 pt-2">
+                  <p className="mb-0 font-weight-bold" style={{ fontSize: 12 }}>
+                    Nickname: {nickname}
+                  </p>
+                  <p className="mb-0">
+                    {status === Status[0] ? (
+                      <AliveIcon width={18} fill="#20c997" />
+                    ) : (
+                      <DeadIcon width={18} fill="#dc3545" />
+                    )}
+                  </p>
+                </div>
+                <div className="px-3 pt-2" style={{ fontSize: 14 }}>
+                  <p className="m-0" style={{ fontSize: 12 }}>
+                    Occupation:
+                  </p>
+                  <Occupation jobs={occupation} />
+                </div>
               </div>
             </div>
           );
