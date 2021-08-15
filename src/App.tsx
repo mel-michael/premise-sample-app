f84cbc4b768f2ae34b6d75f6ad5d75d0a225d562
Mel Michael <mel.michael007@gmail.com>
2021-08-15 11:06:37 -0700

    Add boostrap and initialize landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container pt-3">
+
+      <header>
+        <h3>The Breaking Bad Movie</h3>
       </header>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="btn btn-primary"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <form className="d-flex mt-3">
+        <input className="form-control me-2" type="search" placeholder="Search Characters" aria-label="Search" />
+        <button className="btn btn-outline-info" type="submit">Search</button>
+      </form>
+
+      <div>
+
+      </div>
+
+      <div className="row py-4">
+    <div className="col border m-3">col</div>
+    <div className="col border m-3">col</div>
+    <div className="col border m-3">col</div>
+    <div className="col border m-3">col</div>
+  </div>
     </div>
   );
 }
