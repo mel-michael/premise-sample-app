060612607a7d8386000dcaac25b12b550837c96e
Mel Michael <mel.michael007@gmail.com>
2021-08-16 01:10:12 -0700

    Implement character detail page

--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react';
 import { RouteComponentProps, useParams, Link } from '@reach/router';
 import BeatLoader from 'react-spinners/BeatLoader';
 
-import { Character } from '../Characters/types';
+import { Character, Status } from '../Characters/types';
+
+// images
+import { ReactComponent as DeadIcon } from '../../assets/dead.svg';
+import { ReactComponent as AliveIcon } from '../../assets/alive.svg';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -12,6 +16,20 @@ const CharacterDetails: React.FC<RouteComponentProps> = () => {
   const [error, setError] = useState(false);
 
   const [charData, setCharData] = useState<Character>();
+
+  const Badge = ({ items }: { items: string[] | number[] }): JSX.Element => (
+    <div className="d-flex">
+      {items.map((item) => (
+        <p
+          key={item}
+          style={{ fontSize: 10, fontWeight: 400, marginRight: 10 }}
+          className="badge bg-info text-dark mb-0"
+        >
+          {item}
+        </p>
+      ))}
+    </div>
+  );
 
   useEffect(() => {
     const getAllChars = async () => {
@@ -33,9 +51,10 @@ const CharacterDetails: React.FC<RouteComponentProps> = () => {
 
   return (
     <div className="container">
-      Character Details Page: {charId}
-      <Link to="/">Back to Home</Link>
-      <div className="row pb-6">
+      <p className="mt-3">
+        <Link to="/">Back to Home</Link>
+      </p>
+      <div className="row">
         {loading && (
           <div className="mt-6">
             <BeatLoader color="#ccc" loading={loading} size={16} />
@@ -46,7 +65,40 @@ const CharacterDetails: React.FC<RouteComponentProps> = () => {
 
         {!error && !loading && !charData && <p className="alert alert-info mx-3">No data found for this character</p>}
 
-        {charData && <div>{charData.portrayed}</div>}
+        {charData && (
+          <div className="border shadow-sm mt-6 p-0">
+            <div className="d-flex">
+              <div className="d-flex flex-column align-items-center w-auto">
+                <img src={charData.img} alt="Char" className="img-fluid border" width={300} />
+                <h5 className="my-3 px-2">{charData.portrayed}</h5>
+              </div>
+              <div className="d-flex justify-content-between w-100 p-4">
+                <div>
+                  <h2>{charData.name}</h2>
+                  <span className="fst-italic">Nickname: {charData.nickname} </span>
+                  <div className="my-3">
+                    <p className="">Occupation:</p>
+                    <Badge items={charData.occupation} />
+                  </div>
+                  <div className="my-3">
+                    <p className="">Episodes:</p>
+                    <Badge items={charData.appearance} />
+                  </div>
+                </div>
+                <p>Date of Birth: {charData.birthday}</p>
+                <p className="mb-0">
+                  Status: &nbsp;&nbsp;
+                  {charData.status === Status[0] ? (
+                    <AliveIcon width={18} fill="#20c997" />
+                  ) : (
+                    <DeadIcon width={18} fill="#dc3545" />
+                  )}
+                  &nbsp;&nbsp;{charData.status}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
