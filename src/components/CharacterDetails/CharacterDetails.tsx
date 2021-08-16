7b425cd00813b2e2c57838f46cc910196b788810
Mel Michael <mel.michael007@gmail.com>
2021-08-16 00:29:01 -0700

    Add character details page

--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -1,8 +1,55 @@
-import { RouteComponentProps, useParams } from '@reach/router';
+import { useEffect, useState } from 'react';
+import { RouteComponentProps, useParams, Link } from '@reach/router';
+import BeatLoader from 'react-spinners/BeatLoader';
+
+import { Character } from '../Characters/types';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const CharacterDetails: React.FC<RouteComponentProps> = () => {
   const { charId } = useParams();
-  return <div>Character Details Page: {charId} </div>;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const [charData, setCharData] = useState<Character>();
+
+  useEffect(() => {
+    const getAllChars = async () => {
+      fetch(`${API_URL}/${charId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setCharData(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(true);
+          setLoading(false);
+          console.error(err);
+        });
+    };
+
+    getAllChars();
+  }, [charId]);
+
+  return (
+    <div className="container">
+      Character Details Page: {charId}
+      <Link to="/">Back to Home</Link>
+      <div className="row pb-6">
+        {loading && (
+          <div className="mt-6">
+            <BeatLoader color="#ccc" loading={loading} size={16} />
+          </div>
+        )}
+
+        {error && !loading ? <p className="alert alert-danger mx-3">Oops! Unable to fetch character details</p> : null}
+
+        {!error && !loading && !charData && <p className="alert alert-info mx-3">No data found for this character</p>}
+
+        {charData && <div>{charData.portrayed}</div>}
+      </div>
+    </div>
+  );
 };
 
 export default CharacterDetails;
--- src/components/Characters/CharacterCard.tsx
+++ src/components/Characters/CharacterCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from '@reach/router';
+
 // styles
 import './Characters.css';
 
@@ -14,25 +16,27 @@ type CardProps = {
 const CharacterCard: React.FC<CardProps> = ({ data }) => (
   <>
     {data.map((item) => {
-      const { char_id, name, img, status, nickname } = item;
+      const { char_id, name, img, status, portrayed } = item;
       return (
         <div key={char_id} className="col-3 mb-5">
-          <div data-testid="character-element" className="image-card position-relative rounded shadow-sm p-0">
-            <img src={img} alt="Char" className="img-fluid rounded-top w-100 h-100" style={{ maxHeight: 420 }} />
-            <h6 className="character-name">{name}</h6>
-            <div className="d-flex justify-content-between align-items-center px-3 pt-2">
-              <p className="mb-0 font-weight-bold" style={{ fontSize: 12 }}>
-                Nickname: {nickname}
-              </p>
-              <p className="mb-0">
-                {status === Status[0] ? (
-                  <AliveIcon width={18} fill="#20c997" />
-                ) : (
-                  <DeadIcon width={18} fill="#dc3545" />
-                )}
-              </p>
+          <Link to={`/character/${char_id}`}>
+            <div data-testid="character-element" className="image-card position-relative rounded shadow-sm p-0">
+              <img src={img} alt="Char" className="img-fluid rounded-top w-100 h-100" style={{ maxHeight: 420 }} />
+              <h6 className="character-name">{name}</h6>
+              <div className="d-flex justify-content-between align-items-center px-3 pt-2">
+                <p className="mb-0 font-weight-bold" style={{ fontSize: 12 }}>
+                  Actor: <strong>{portrayed}</strong>
+                </p>
+                <p className="mb-0">
+                  {status === Status[0] ? (
+                    <AliveIcon width={18} fill="#20c997" />
+                  ) : (
+                    <DeadIcon width={18} fill="#dc3545" />
+                  )}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       );
     })}
