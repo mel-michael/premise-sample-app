a812d302217ed470ea1e807f0c5adc59f1513455
Mel Michael <mel.michael007@gmail.com>
2021-08-16 01:28:01 -0700

    Adjust styling

--- src/components/Characters/CharacterCard.tsx
+++ src/components/Characters/CharacterCard.tsx
@@ -3,11 +3,7 @@ import { Link } from '@reach/router';
 // styles
 import './Characters.css';
 
-// images
-import { ReactComponent as DeadIcon } from '../../assets/dead.svg';
-import { ReactComponent as AliveIcon } from '../../assets/alive.svg';
-
-import { Status, Character } from './types';
+import { Character } from './types';
 
 type CardProps = {
   data: Character[];
@@ -16,25 +12,13 @@ type CardProps = {
 const CharacterCard: React.FC<CardProps> = ({ data }) => (
   <>
     {data.map((item) => {
-      const { char_id, name, img, status, portrayed } = item;
+      const { char_id, name, img } = item;
       return (
         <div key={char_id} className="col-3 mb-5">
           <Link to={`/character/${char_id}`}>
             <div data-testid="character-element" className="image-card position-relative rounded shadow-sm p-0">
-              <img src={img} alt="Char" className="img-fluid rounded-top w-100 h-100" style={{ maxHeight: 420 }} />
+              <img src={img} alt="Char" className="img-fluid image-overlay shadow" style={{ maxHeight: 420 }} />
               <h6 className="character-name">{name}</h6>
-              <div className="d-flex justify-content-between align-items-center px-3 pt-2">
-                <p className="mb-0 font-weight-bold" style={{ fontSize: 12 }}>
-                  Actor: <strong>{portrayed}</strong>
-                </p>
-                <p className="mb-0">
-                  {status === Status[0] ? (
-                    <AliveIcon width={18} fill="#20c997" />
-                  ) : (
-                    <DeadIcon width={18} fill="#dc3545" />
-                  )}
-                </p>
-              </div>
             </div>
           </Link>
         </div>
